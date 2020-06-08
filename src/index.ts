d86ec0a44f8e2daa6af9a8f650c4a016b24b49e9
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-06-08 17:34:25 +0530

    Add project dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world!")
+import express from "express";
+
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.send(`
+    <div>
+      Hello There!
+    </div>
+  `);
+})
+
+app.listen(3000,()=>{
+  console.log("Fired at http://localhost:3000")
+})
