46108e0a9fd3fd8c11f2e54202314377501f2892
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-06-10 12:28:12 +0530

    Login route handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { loginRouter } from "./routes/login.route";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/login',loginRouter)
 
 app.listen(3000,()=>{
--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -2,12 +2,28 @@ import { Router, Request, Response } from "express";
 
 const loginRouter = Router();
 
-loginRouter.get('/',(req: Request, res: Response): void=>{
-    res.send(`
-    <div>
-      Hello There!
-    </div>
-  `)
-});
+loginRouter.route("/")
+  .get((req: Request, res: Response): void=>{
+      res.send(`
+      <form method="POST">
+        <div>
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email">
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password">
+        </div>
+        <button>Submit</button>
+      </form>
+    `)
+  })
+  .post((req: Request, res: Response): void =>{
+    const { email, password } = req.body;
+    res.send({
+      email,
+      password
+    })
+  })
 
 export { loginRouter }
