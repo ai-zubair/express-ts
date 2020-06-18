f775e952d4108abecadda8208d1bab8ecff2e724
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-06-18 19:16:30 +0530

    Add HTTP Handler factories

--- src/decorators/controller.dec.ts
+++ src/decorators/controller.dec.ts
@@ -1,14 +1,20 @@
 import "reflect-metadata";
 import { AppRouter } from "../AppRouter";
+import { HttpMethods } from "./constants";
 
 export const controller = (route: string): ClassDecorator => {
   return function controllerDec( target: Function ): void{
+    
     const classPrototype = target.prototype;
     const appRouter = AppRouter.getRouter();
+
     for (const memberKey in classPrototype) {
+     
       const path = Reflect.getMetadata("path", classPrototype, memberKey);
-      if(path){
-        appRouter.get(`${route}${path}`,classPrototype[memberKey])
+      const method: HttpMethods = Reflect.getMetadata("method", classPrototype, memberKey);
+      
+      if(path && method){
+        appRouter[method](`${route}${path}`,classPrototype[memberKey])
       }
     }
   }
--- src/decorators/handler.dec.ts
+++ src/decorators/handler.dec.ts
@@ -1,7 +1,20 @@
 import "reflect-metadata";
+import { HttpMethods } from "./constants";
 
-export const get = (path: string): MethodDecorator => {
-  return function getDec(target: Object, key: string | symbol, desc: PropertyDescriptor){
-    Reflect.defineMetadata("path", path, target, key)
+interface MethodDecoratorFactory{
+  (path: string): MethodDecorator;
+}
+
+const httpHandler = (httpMethod: string): MethodDecoratorFactory => {
+  return function handlerFactory(path: string): MethodDecorator{
+    return function handlerDec(target: Object, key: string | symbol, desc: PropertyDescriptor){
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", httpMethod, target, key);
+    }
   }
 }
+
+export const get = httpHandler(HttpMethods.GET);
+export const post = httpHandler(HttpMethods.POST);
+export const put = httpHandler(HttpMethods.PUT);
+export const del = httpHandler(HttpMethods.DELETE);
