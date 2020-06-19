62facd34245ac84408f83e8a51d082b8182764b6
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-06-19 11:59:51 +0530

    Add login/logout handlers

--- src/controllers/Login.controller.ts
+++ src/controllers/Login.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
-import { get, controller, use } from "../decorators";
+import { get, controller, use, post } from "../decorators";
 import { authenticate } from "../middlewares/middlewares";
+
+interface RequestWithBody extends Request{
+  body: { [key:string]: string|undefined }
+}
 
 @controller("/auth")
 class LoginController{
 
   @get("/login")
-  @use([authenticate])
   getLogin(req: Request, res: Response): void{
     if(req.session && req.session.isAuthenticated){
       res.redirect("/");
@@ -27,6 +30,23 @@ class LoginController{
     }
   }
 
+  @post("/login")
+  postLogin(req: RequestWithBody, res: Response): void {
+    const { email, password } = req.body;
+    if(email && password && email === "x@x.com" && password === "123" && req.session){
+      req.session.isAuthenticated = true;
+      res.redirect('/');
+    }else{
+      res.send("Invalid email/password")
+    }
+  }
+
+  @get('/logout')
+  @use([authenticate])
+  getLogout(req: Request, res: Response): void {
+    req.session = null;
+    res.redirect('/auth/login')
+  }
 }
 
 export { LoginController }
